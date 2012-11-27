2cdff73d1eaca9c9c2b921979a639d7bfc11fdeb
mkutis <mkutis@macalester.edu>
2012-11-27 02:41:31 -0600

    here we go again

---
+++ Chronolist/src/timeline/chronolist/Timeline.java
@@ -0,0 +1,22 @@
+package timeline.chronolist;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Timeline extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_timeline);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_timeline, menu);
+		return true;
+	}
+
+}
